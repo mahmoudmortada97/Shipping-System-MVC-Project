cce833392138ea023905e0f82f8c866d289e85c8
RizkAhmed <rizkbatah26@yahoo.com>
2023-06-19 22:48:50 +0300

    now

--- MVCProject/ViewModel/RepresentativeGovBranchPercentageViewModel.cs
+++ MVCProject/ViewModel/RepresentativeGovBranchPercentageViewModel.cs
@@ -9,34 +9,44 @@ namespace MVCProject.ViewModel
         [Key]
         public int Id { get; set; }
 
-        [MaxLength(50)]
-        [MinLength(8, ErrorMessage = "Name must be more than 8 char")]
+        [MaxLength(50,
+            ErrorMessage = "Name must be less than 50 characters")]
+        [MinLength(8, 
+            ErrorMessage = "Name must be more than 8 characters")]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$", 
+            ErrorMessage = "Email address is not in valid")]
+
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
         [RegularExpression(@"(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,16})$",
-                    ErrorMessage = "Password must be 8-16 characters long</br> with at least one numeric character")]
+                    ErrorMessage = "Password must be 8 : 16 characters long</br> with at least one numeric character")]
         public string Password { get; set; }
 
         public bool IsDeleted { get; set; } = false;
 
-        [RegularExpression(@"^01[0125][0-9]{8}$", ErrorMessage = "The Phone number field is not valid")]
+        [RegularExpression(@"^01[0125][0-9]{8}$", 
+            ErrorMessage = "The Phone number field is not valid")]
         public string Phone { get; set; }
 
-        [MinLength(5)]
-        [MaxLength(50)]
+        [MinLength(5, 
+            ErrorMessage = "Address must be more than 5 characters")]
+        [MaxLength(50,
+            ErrorMessage = "Address must be less than 50 characters")]
         public string Address { get; set; }
 
         public decimal CompanyPercentageOfOrder { get; set; }
 
-        [Required(ErrorMessage = "Please choose governorate")]
+        [Required(
+            ErrorMessage = "Please choose governorate")]
         public int GovernorateId { get; set; }
         public  List<Governorate> Governorates { get; set; } //for drop down list
 
-        [Required(ErrorMessage = "Please choose branch")]
+        [Required(
+            ErrorMessage = "Please choose branch")]
         public int BranchId { get; set; }
         public List<Branch> Branchs { get; set; } //for drop down list
         public int? DiscountTypeId { get; set; }
