752a242ae612ec29a2576855b3461b265f107f51
Mohamednabil96 <mohamednabilgomaa96@gmail.com>
2023-06-24 19:59:28 +0300

    relation between employee and branch added

--- MVCProject/Models/Branch.cs
+++ MVCProject/Models/Branch.cs
@@ -14,6 +14,7 @@ namespace MVCProject.Models
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
         public bool IsDeleted { get; set; } = false;
-        public List<Trader> Traders { get; set; }
+        public List<Trader> Traders { get; set; } = new List<Trader>();
+        public List <Employee> Employees { get; set; } = new List<Employee>();
     }
 }
