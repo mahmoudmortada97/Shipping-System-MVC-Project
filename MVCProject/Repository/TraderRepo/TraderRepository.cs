2e205a3556f054cb81308525b00b60b28341eedb
Mahmoud Mortada <mahmoudtaha654@gmail.com>
2023-06-24 17:45:44 +0300

    Make EditOrder

--- MVCProject/Repository/TraderRepo/TraderRepository.cs
+++ MVCProject/Repository/TraderRepo/TraderRepository.cs
@@ -9,8 +9,8 @@ namespace MVCProject.Repository.TraderRepo
     {
         AppDbContext _context;
 
-<<<<<<< HEAD
-=======
+
+
         //public List<Trader> traders = new List<Trader>()
         //    {
 
@@ -46,7 +46,7 @@ namespace MVCProject.Repository.TraderRepo
 
         //    };
 
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
         public TraderRepository(AppDbContext context)
         {
             _context = context;
@@ -54,57 +54,57 @@ namespace MVCProject.Repository.TraderRepo
 
         public List<Trader> GetAll()
         {
-<<<<<<< HEAD
+
             return _context.Traders.Where(e => e.IsDeleted == false).ToList(); //uncommented
-=======
+
             return _context.Traders.Where(e => e.IsDeleted == false).ToList();
 
             //return traders.Where(e => e.IsDeleted == false).ToList();
 
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
         }
 
 
         public Trader GetById(int id)
         {
-<<<<<<< HEAD
+
 
             return _context.Traders.FirstOrDefault(e => e.Id == id && e.IsDeleted == false)!;//uncommented
 
             //return traders.FirstOrDefault(t => t.Id == id && t.IsDeleted == true)!; //commented
 
-=======
+
             return _context.Traders.FirstOrDefault(e => e.Id == id && e.IsDeleted == true)!;
             //return traders.FirstOrDefault(t => t.Id == id && t.IsDeleted == true)!;
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
         }
 
         public void Create(Trader trader) //changed from (Add --> Create) in all places 
         {
-<<<<<<< HEAD
+
             _context.Traders.Add(trader); //uncommented
-=======
+
             _context.Traders.Add(trader);
 
             //traders.Add(trader);
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
 
         }
 
         public void Edit(Trader trader)
         {
-<<<<<<< HEAD
+
 
             //var traderFromDb = traders.FirstOrDefault(t => t.Id == trader.Id); //commented
 
             _context.Traders.Entry(trader).State = Microsoft.EntityFrameworkCore.EntityState.Modified; //uncommented
             //var traderFromDb = traders.FirstOrDefault(t => t.Id == trader.Id);
 
-=======
+
             _context.Traders.Entry(trader).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
             //var traderFromDb = traders.FirstOrDefault(t => t.Id == trader.Id);
 
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
             //traderFromDb.Name = trader.Name;
             //traderFromDb.Email = trader.Email;
             //traderFromDb.Password = trader.Password;
@@ -113,13 +113,13 @@ namespace MVCProject.Repository.TraderRepo
             //traderFromDb.Address = trader.Address;
             //traderFromDb.StoreName = trader.StoreName;
             //traderFromDb.SpecialPickupCost = trader.SpecialPickupCost;
-<<<<<<< HEAD
+
 
             //traderFromDb.TraderTaxForRejectedOrders = trader.TraderTaxForRejectedOrders;  //commented
 
-=======
+
             //traderFromDb.TraderTaxForRejectedOrders = trader.TraderTaxForRejectedOrders;
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
 
             //Id = 1,
             //        Name = "Ali",
@@ -136,15 +136,13 @@ namespace MVCProject.Repository.TraderRepo
 
         public void Delete(int id)
         {
-<<<<<<< HEAD
-            Trader trader = _context.Traders.Find(id)!; //uncommented
-            trader.IsDeleted = true; //uncommented
-=======
+
+
             Trader trader = _context.Traders.Find(id)!;
             trader.IsDeleted = true;
             //var trader = traders.FirstOrDefault(t => t.Id == id);
             //trader.IsDeleted = true;
->>>>>>> aaf6d917aed8a49390cb37fa016f1db0c536f0f1
+
 
             //var trader = traders.FirstOrDefault(t => t.Id == id); //commented
             //trader.IsDeleted = true;//commented
@@ -155,5 +153,6 @@ namespace MVCProject.Repository.TraderRepo
             _context.SaveChanges();
         }
 
+     
     }
 }
